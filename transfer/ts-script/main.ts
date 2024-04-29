1158468308a98fe5de8a0006d498d893e3710334
eth2the-moon <278338249@qq.com>
2024-04-30 02:07:24 +0800

    add

---
+++ transfer/ts-script/main.ts
@@ -0,0 +1,90 @@
+import { ethers } from "ethers";
+import {
+  loadDeployedAddresses,
+  getWallet,
+  wait,
+  loadConfig,
+  storeDeployedAddresses,
+  getChain,
+} from "./utils";
+import {
+  ChainId,
+  attestFromEth,
+  createWrappedOnEth,
+  getSignedVAAWithRetry,
+  parseSequenceFromLogEth,
+  tryNativeToHexString,
+} from "@certusone/wormhole-sdk";
+import * as grpcWebNodeHttpTransport from "@improbable-eng/grpc-web-node-http-transport";
+import { ChainInfo, getArg } from "./utils";
+
+const sourceChain = loadConfig().sourceChain;
+const targetChain = loadConfig().targetChain;
+
+export async function deployMockToken() {
+  const deployed = loadDeployedAddresses();
+  const from = getChain(sourceChain);
+
+  // const signer = getWallet(from.chainId);
+  // const HT = await new ERC20Mock__factory(signer).deploy("HelloToken", "HT");
+  // await HT.deployed();
+  // console.log(`HT deployed to ${HT.address} on chain ${from.chainId}`);
+  // deployed.erc20s[sourceChain] = [HT.address];
+
+  // console.log("Minting...");
+  // await HT.mint(signer.address, ethers.utils.parseEther("1000")).then(wait);
+  // console.log("Minted 1000 HT to signer");
+
+  // console.log(
+  //   `Attesting tokens with token bridge on chain(s) ${loadConfig()
+  //     .chains.map((c) => c.chainId)
+  //     .filter((c) => c === targetChain)
+  //     .join(", ")}`
+  // );
+  for (const chain of loadConfig().chains) {
+    if (chain.chainId !== targetChain) {
+      continue;
+    }
+    await attestWorkflow({
+      from: getChain(sourceChain),
+      to: chain,
+      token: "0xFd57b4ddBf88a4e07fF4e34C487b99af2Fe82a05",
+    });
+  }
+
+  // storeDeployedAddresses(deployed);
+}
+
+
+async function attestWorkflow({
+    to,
+    from,
+    token,
+  }: {
+    to: ChainInfo;
+    from: ChainInfo;
+    token: string;
+  }) {
+    const attestRx: ethers.ContractReceipt = await attestFromEth(
+      from.tokenBridge!,
+      getWallet(from.chainId),
+      token
+    );
+    const seq = parseSequenceFromLogEth(attestRx, from.wormhole);
+  
+    const res = await getSignedVAAWithRetry(
+      ["https://api.testnet.wormscan.io"],
+      Number(from) as ChainId,
+      tryNativeToHexString(from.tokenBridge, "ethereum"),
+      seq.toString(),
+      { transport: grpcWebNodeHttpTransport.NodeHttpTransport() }
+    );
+    const createWrappedRx = await createWrappedOnEth(
+      to.tokenBridge,
+      getWallet(to.chainId),
+      res.vaaBytes
+    );
+    console.log(
+      `Attested token from chain ${from.chainId} to chain ${to.chainId}`
+    );
+  }
